1463436d7a55f9947970f0b66b3633e796cccec8
darento <dsartiom@gmail.com>
2024-01-31 10:39:03 +0100

    Redactoring and generalized for FEM128 and FEM256.

--- src/map_generator.py
+++ src/map_generator.py
@@ -1,65 +1,141 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import yaml
+
+from src.plots import plot_chan_position
 
 
 def _read_yaml_file(mapping_file: str) -> dict:
     """
-    Read and return yaml mapping file.
+    This function reads a YAML file and returns its contents as a dictionary.
+
+    Parameters:
+    mapping_file (str): The path to the YAML file.
+
+    Output:
+    dict: The contents of the YAML file as a dictionary.
+
+    Raises:
+    RuntimeError: If the YAML file is not readable or not found, or if it does not have the correct format.
     """
     try:
         with open(mapping_file) as map_buffer:
-            channel_map = yaml.safe_load(map_buffer)
+            yaml_map = yaml.safe_load(map_buffer)
     except yaml.YAMLError:
         raise RuntimeError("Mapping file not readable.")
     except FileNotFoundError:
         raise RuntimeError("Mapping file not found, please check directory")
     if (
-        type(channel_map) is not dict
-        or "channels_j1" not in channel_map.keys()
-        or "channels_j2" not in channel_map.keys()
+        type(yaml_map) is not dict
+        or "channels_j1" not in yaml_map.keys()
+        or "channels_j2" not in yaml_map.keys()
+        or "FEM" not in yaml_map.keys()
+        or "FEBD" not in yaml_map.keys()
+        or "mod_feb_map" not in yaml_map.keys()
+        or "x_pitch" not in yaml_map.keys()
+        or "y_pitch" not in yaml_map.keys()
     ):
         raise RuntimeError("Mapping file not correct format.")
-    return channel_map
+    return yaml_map
 
 
-# Function to convert channel numbers to x, y coordinates
-def get_coordinates(mapping_file: str) -> dict:
-    channel_map = _read_yaml_file(mapping_file)
+def _get_coordinates_FEM128(channel_pos: int, x_pitch: float, y_pitch: float) -> tuple:
+    """
+    This function calculates the x and y coordinates for a given channel position in the FEM128.
 
-    print(channel_map)
+    Parameters:
+    channel_pos (int): The position of the channel.
+    x_pitch (float): The x pitch.
+    y_pitch (float): The y pitch.
 
-    exit(0)
-    """# Calculate row and column in the grid
-    row = (channel_num) // 8
-    col = (channel_num) % 8
+    Output:
+    tuple: The x and y coordinates.
+    """
+    # Calculate row and column in the grid
+    row = (channel_pos) // 8
+    col = (channel_pos) % 8
 
     # Calculate x and y coordinates
-    x = (col + 0.5) * distance_between_channels
-    y = (row + 0.5) * distance_between_channels
+    loc_x = (col + 0.5) * x_pitch
+    loc_y = (row + 0.5) * y_pitch
 
-    print(f"channel: {channel_num}, row: {row}, col: {col}, x: {x}, y: {y}")
-    return (x, y)"""
+    print(f"channel: {channel_pos}, row: {row}, col: {col}, x: {loc_x}, y: {loc_y}")
+    return (loc_x, loc_y)
 
 
-def plot_chan_position(channels: list) -> None:
-    # Initialize plot
-    fig, ax = plt.subplots(figsize=(10, 10))
+def _get_coordinates_FEM256(channel_pos: int, x_pitch: float, y_pitch: float) -> tuple:
+    pass
 
-    # Distance between channels
-    distance = 3  # in millimeters
 
-    # Plot each channel for J1
-    for i, channel in enumerate(channels):
-        x, y = get_coordinates(i, distance)
-        ax.scatter(x, y, label=f"Channel {channel}")
-        ax.text(x, y, str(channel), va="center", ha="center")
+def _get_local_mapping(
+    FEM_chan: int,
+    mod_feb_map: dict,
+    channels_j1: list,
+    channels_j2: list,
+    x_pitch: float,
+    y_pitch: float,
+) -> dict:
+    """
+    This function generates the basic mapping from the mapping file.
 
-    # Similarly plot for J2 and any other ports if needed
+    Parameters:
+    FEM_chan (int): The number of channels in the FEM.
+    mod_feb_map (dict): The mapping from modules to FEBs.
+    channels_j1 (list): The list of channels in J1.
+    channels_j2 (list): The list of channels in J2.
+    x_pitch (float): The x pitch.
+    y_pitch (float): The y pitch.
 
-    # Set plot properties
-    ax.set_xlabel("X position (mm)")
-    ax.set_ylabel("Y position (mm)")
-    ax.set_title("Floodmap Representation of Channels")
-    ax.axis("equal")  # To maintain the aspect ratio
-    plt.show()
+    Output:
+    dict: The local mapping.
+    """
+    get_coord_from = (
+        _get_coordinates_FEM128 if FEM_chan == 128 else _get_coordinates_FEM256
+    )
+    local_map = {}
+    for mod, value in mod_feb_map.items():
+        portID, slaveID, febport = value
+        # Calculate the minimum channel number for the module
+        mod_min_chan = 131072 * portID + 4096 * slaveID + FEM_chan * febport
+        for i, (ch_j1, ch_j2) in enumerate(zip(channels_j1, channels_j2)):
+            loc_x, loc_y = get_coord_from(i, x_pitch, y_pitch)
+            local_map[ch_j1 + mod_min_chan] = (loc_x, loc_y)
+            local_map[ch_j2 + mod_min_chan] = (loc_x, loc_y)
+    print(local_map)
+    return local_map
+
+
+def map_factory(mapping_file: str) -> dict:
+    """
+    This function reads a mapping file and generates a mapping.
+
+    Parameters:
+    mapping_file (str): The path to the mapping file.
+
+    Output:
+    dict: The mapping.
+
+    Raises:
+    RuntimeError: If the FEM is not in the correct format.
+    """
+    yaml_map = _read_yaml_file(mapping_file)
+    FEM = yaml_map["FEM"]
+    FEBD = yaml_map["FEBD"]
+    x_pitch = yaml_map["x_pitch"]
+    y_pitch = yaml_map["y_pitch"]
+    mod_feb_map = yaml_map["mod_feb_map"]
+
+    if FEM == "FEM128":
+        FEM_chan = 128
+        channels_j1 = yaml_map["channels_j1"]
+        channels_j2 = yaml_map["channels_j2"]
+        local_map = _get_local_mapping(
+            FEM_chan, mod_feb_map, channels_j1, channels_j2, x_pitch, y_pitch
+        )
+    elif FEM == "FEM256":
+        # TODO: Add FEM256 mapping
+        FEM_chan = 256
+        pass
+    else:
+        raise RuntimeError("FEM not correct format.")
+
+    return local_map
