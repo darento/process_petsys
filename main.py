163768f28337e86fc9d50c193c2cfad26af87e97
darento <dsartiom@gmail.com>
2024-02-02 12:07:47 +0100

    Working on plot of each chipID channel position, debug purpouse maybe.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from src.read_compact import read_binary_file
 from src.filters import filter_total_energy
 import src.filters as filters
 
-from src.detector_features import calculate_total_energy
+from src.detector_features import calculate_centroid, calculate_total_energy
 from src.mapping_generator import map_factory
 from src.plots import plot_chan_position
 
@@ -39,10 +39,10 @@ def main():
     map_file = config["map_file"]
 
     # Get the coordinates of the channels
-    dict_coords = map_factory(map_file)
+    dict_coords, num_ASICs = map_factory(map_file)
 
     # Plot the coordinates of the channels
-    plot_chan_position(dict_coords)
+    plot_chan_position(dict_coords, num_ASICs)
 
     # Read the energy range
     en_min = float(config["energy_range"][0])
@@ -60,6 +60,8 @@ def main():
         det1, det2 = event
         det1_en = calculate_total_energy(det1)
         en_filter = filter_total_energy(det1_en, en_min, en_max)
+        if en_filter:
+            calculate_centroid(det1, 1, 1)
         # print(f"En filter: {filter_total_energy(det1, 50)}")
         # print(f"Lenghts: {len(det1)}, {len(det2)}")
         # time.sleep(1)
--- src/plots.py
+++ src/plots.py
@@ -1,8 +1,11 @@
-from collections import defaultdict
+import math
 from matplotlib import pyplot as plt
 
+from src.detector_features import get_electronics_nums
 
-def plot_chan_position(dict_coords: dict) -> None:
+
+def plot_chan_position(dict_coords: dict, mod_number) -> None:
+    # TODO: Save figure per chipID ? Is it useful?
     """
     This function plots the position of the channels on a graph.
 
@@ -13,29 +16,38 @@ def plot_chan_position(dict_coords: dict) -> None:
     Output:
     None. This function plots a graph and does not return anything.
     """
+    sqrt_mod = math.ceil(mod_number**0.5)
+
     # Initialize plot
-    fig, ax = plt.subplots(figsize=(10, 10))
+    fig, ax = plt.subplots(sqrt_mod, sqrt_mod, figsize=(10, 10))
 
     # Create a dictionary to store the count of channels at each position
-    position_counts = defaultdict(int)
+    mM_dict = {}
+    fig_number = 0
 
     # Plot each channel
     for ch, xy in dict_coords.items():
-        x, y = xy
-        ax.scatter(x, y, label=f"Channel {ch}")
+        portID, slaveID, chipID, channelID = get_electronics_nums(ch)
+        if (portID, slaveID, chipID) not in mM_dict:
+            mM_dict[(portID, slaveID, chipID)] = fig_number
+            fig_number += 1
 
-        # Increment the count
-        position_counts[xy] += 1
+        # Calculate the row and column indices
+        row_index = mM_dict[(portID, slaveID, chipID)] // sqrt_mod
+        col_index = mM_dict[(portID, slaveID, chipID)] % sqrt_mod
+
+        x, y = xy
+        print(mM_dict[(portID, slaveID, chipID)], row_index, col_index)
+        ax[row_index, col_index].scatter(x, y, label=f"Channel {ch}")
 
         # Adjust the text position based on the count
-        offset = 0.35 * position_counts[xy]
-        ax.text(x, y + offset, str(ch), va="center", ha="center")
+        ax[row_index, col_index].text(x, y, str(ch), va="center", ha="center")
 
-    # Similarly plot for J2 and any other ports if needed
+        # Similarly plot for J2 and any other ports if needed
 
-    # Set plot properties
-    ax.set_xlabel("X position (mm)")
-    ax.set_ylabel("Y position (mm)")
-    ax.set_title("Floodmap Representation of Channels")
-    ax.axis("equal")  # To maintain the aspect ratio
+        # Set plot properties
+        ax[row_index, col_index].set_xlabel("X position (mm)")
+        ax[row_index, col_index].set_ylabel("Y position (mm)")
+        ax[row_index, col_index].set_title("Floodmap Representation of Channels")
+        ax[row_index, col_index].axis("equal")  # To maintain the aspect ratio
     plt.show()
