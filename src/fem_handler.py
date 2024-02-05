30e2aa064ac439fb349126d8e965e270ab718914
darento <dsartiom@gmail.com>
2024-02-05 16:17:01 +0100

    Added row_col_sum flag depending on the FE sensor board, also added channels per mM.

--- src/fem_handler.py
+++ src/fem_handler.py
@@ -1,8 +1,18 @@
 class FEMBase:
-    def __init__(self, x_pitch: float, y_pitch: float, channels: int, num_ASICS: int):
+    def __init__(
+        self,
+        x_pitch: float,
+        y_pitch: float,
+        sum_rows_cols: bool,
+        channels: int,
+        mM_channels: int,
+        num_ASICS: int,
+    ):
         self.x_pitch = x_pitch
         self.y_pitch = y_pitch
+        self.sum_rows_cols = sum_rows_cols
         self.channels = channels
+        self.mM_channels = mM_channels
         self.num_ASICS = num_ASICS
 
     def get_coordinates(self, channel_pos: int) -> tuple:
@@ -10,30 +20,47 @@ class FEMBase:
 
 
 class FEM128(FEMBase):
-    def __init__(self, x_pitch: float, y_pitch: float):
-        super().__init__(x_pitch, y_pitch, 128, 2)
+    def __init__(
+        self, x_pitch: float, y_pitch: float, mM_channels: int, sum_rows_cols: bool
+    ):
+        super().__init__(x_pitch, y_pitch, sum_rows_cols, 128, mM_channels, 2)
 
     def get_coordinates(self, channel_pos: int) -> tuple:
-        row = channel_pos // 8
-        col = channel_pos % 8
-        loc_x = (col + 0.5) * self.x_pitch
-        loc_y = (row + 0.5) * self.y_pitch
+        if not self.sum_rows_cols:
+            row = channel_pos // 8
+            col = channel_pos % 8
+            loc_x = (col + 0.5) * self.x_pitch
+            loc_y = (row + 0.5) * self.y_pitch
+        else:
+            loc_x = self.x_pitch / 2 + self.x_pitch * (channel_pos % 16)
+            loc_y = self.y_pitch / 2 + self.y_pitch * (7 - channel_pos % 16)
         return (loc_x, loc_y)
 
 
 class FEM256(FEMBase):
-    def __init__(self, x_pitch: float, y_pitch: float):
-        super().__init__(x_pitch, y_pitch, 256, 4)
+    def __init__(
+        self, x_pitch: float, y_pitch: float, mM_channels: int, sum_rows_cols: bool
+    ):
+        super().__init__(x_pitch, y_pitch, sum_rows_cols, 256, mM_channels, 4)
 
     def get_coordinates(self, channel_pos: int) -> tuple:
-        # Implement FEM256 logic
-        pass
+        if not self.sum_rows_cols:
+            row = channel_pos // 16
+            col = channel_pos % 16
+            loc_x = round((col + 0.5) * self.x_pitch, 2)
+            loc_y = round((row + 0.5) * self.y_pitch, 2)
+        else:
+            loc_x = round(self.x_pitch / 2 + self.x_pitch * (channel_pos % 32), 2)
+            loc_y = round(self.y_pitch / 2 + self.y_pitch * (31 - channel_pos % 32), 2)
+        return (loc_x, loc_y)
 
 
-def get_FEM_instance(FEM_type: str, x_pitch: float, y_pitch: float):
+def get_FEM_instance(
+    FEM_type: str, x_pitch: float, y_pitch: float, mM_channels: int, sum_rows_cols: bool
+) -> FEMBase:
     if FEM_type == "FEM128":
-        return FEM128(x_pitch, y_pitch)
+        return FEM128(x_pitch, y_pitch, mM_channels, sum_rows_cols)
     elif FEM_type == "FEM256":
-        return FEM256(x_pitch, y_pitch)
+        return FEM256(x_pitch, y_pitch, mM_channels, sum_rows_cols)
     else:
         raise ValueError("Unsupported FEM type")
