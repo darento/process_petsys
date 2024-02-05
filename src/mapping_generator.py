b540bd6debf5e8c6b3f6d46f023ddd09a1d32a83
darento <dsartiom@gmail.com>
2024-02-05 16:18:34 +0100

    Added sm_mM_map dictionary to track mM and sm per channel, helps on data analysis.

--- src/mapping_generator.py
+++ src/mapping_generator.py
@@ -3,8 +3,12 @@ from src.yaml_handler import YAMLMapReader, get_optional_group_keys
 
 
 def _get_local_mapping(
-    mod_feb_map: dict, channels_1: list, channels_2: list, FEM_instance: FEMBase
+    mod_feb_map: dict,
+    channels_1: list,
+    channels_2: list,
+    FEM_instance: FEMBase,
 ) -> dict:
+    # TODO: Need to generalize this function to handle slabs and other types of detectors.
     """
     Generates the basic mapping from the mapping file.
 
@@ -18,17 +22,26 @@ def _get_local_mapping(
     - dict: The local mapping.
     """
     local_map = {}
+    sm_mM_map = {}
+
     for mod, value in mod_feb_map.items():
         portID, slaveID, febport = value
         mod_min_chan = (
             131072 * portID + 4096 * slaveID + FEM_instance.channels * febport
         )
         for i, (ch_1, ch_2) in enumerate(zip(channels_1, channels_2)):
+            # if sum_rows_cols is True, the number of channels is divided by 2 as we have half the number of channels per ASIC
+            # for ch_1 and ch_2
             loc_x, loc_y = FEM_instance.get_coordinates(i)
             local_map[ch_1 + mod_min_chan] = (loc_x, loc_y)
             local_map[ch_2 + mod_min_chan] = (loc_x, loc_y)
+            mM = int(i // FEM_instance.mM_channels)
+            sm_mM_map[ch_1 + mod_min_chan] = (mod, mM)
+            sm_mM_map[ch_2 + mod_min_chan] = (
+                (mod, mM + 1) if not FEM_instance.sum_rows_cols else (mod, mM)
+            )
 
-    return local_map
+    return local_map, sm_mM_map
 
 
 def map_factory(mapping_file: str) -> dict:
@@ -49,6 +62,8 @@ def map_factory(mapping_file: str) -> dict:
             "mod_feb_map": dict,
             "x_pitch": (int, float),
             "y_pitch": (int, float),
+            "mM_channels": int,
+            "sum_rows_cols": bool,
         },
         "optional_groups": [
             {"group": ["channels_j1", "channels_j2"], "type": list},
@@ -59,9 +74,12 @@ def map_factory(mapping_file: str) -> dict:
     reader = YAMLMapReader(yaml_schema)
     yaml_map = reader.read_yaml_file(mapping_file)
     FEM_type = yaml_map["FEM"]
+    mM_channels = yaml_map["mM_channels"]
+    sum_rows_cols = yaml_map["sum_rows_cols"]
     x_pitch = yaml_map["x_pitch"]
     y_pitch = yaml_map["y_pitch"]
     mod_feb_map = yaml_map["mod_feb_map"]
+    mM_disposition = yaml_map["mM_disposition"]
 
     # Assuming get_optional_group_keys logic is included here or adjusted as needed
     channel_group_keys = get_optional_group_keys(
@@ -70,8 +88,15 @@ def map_factory(mapping_file: str) -> dict:
     channels_1 = yaml_map[channel_group_keys[0]]
     channels_2 = yaml_map[channel_group_keys[1]]
 
-    FEM_instance = get_FEM_instance(FEM_type, x_pitch, y_pitch)
+    FEM_instance = get_FEM_instance(
+        FEM_type, x_pitch, y_pitch, mM_channels, sum_rows_cols
+    )
 
     # Implement _get_local_mapping based on the refactored approach
-    local_map = _get_local_mapping(mod_feb_map, channels_1, channels_2, FEM_instance)
-    return local_map, len(mod_feb_map.keys()) * FEM_instance.num_ASICS
+    local_map, sm_mM_map = _get_local_mapping(
+        mod_feb_map,
+        channels_1,
+        channels_2,
+        FEM_instance,
+    )
+    return local_map, sm_mM_map, len(mod_feb_map.keys()) * FEM_instance.num_ASICS
