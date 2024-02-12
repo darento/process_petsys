e2b7c86c72bfacf7d7e90cce22c972e96367894f
darento <dsartiom@gmail.com>
2024-02-12 10:41:12 +0100

    Added ROI filter.

--- src/filters.py
+++ src/filters.py
@@ -132,3 +132,19 @@ def filter_channel_list(
     return all(imp[0] in valid_channels for imp in det1_list) or all(
         imp[0] in valid_channels for imp in det2_list
     )
+
+
+def filter_ROI(x_pos: float, y_pos: float, x_ROI: float, y_ROI: float) -> bool:
+    """
+    Filters events based on the position of the event.
+
+    Parameters:
+    x_pos (float): The x position of the event.
+    y_pos (float): The y position of the event.
+    x_ROI (float): The x position of the region of interest.
+    y_ROI (float): The y position of the region of interest.
+
+    Returns:
+    bool: True if the event is within the region of interest, False otherwise.
+    """
+    return (x_pos > x_ROI) & (x_pos < x_ROI) & (y_pos > y_ROI) & (y_pos < y_ROI)
