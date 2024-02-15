9598a72a95d7fcbff84ed7dc87c541d72d673757
alvaroanreus <coque.aav.04@gmail.com>
2024-02-15 12:11:41 +0100

    Correction filters_test

--- src/filters_test.py
+++ src/filters_test.py
@@ -5,26 +5,45 @@ from .filters import (
     filter_max_sm,
     filter_specific_mm,
     filter_channel_list,
+    filter_ROI,
 )
-
+from enum import Enum, auto
+from .mapping_generator import ChannelType
 import numpy as np
+
+det_list = [
+    [1033333333333333333, 11.0, 1],
+    [1555555555555555555, 33.0, 2],
+    [4666666666666666666, 15.0, 3],
+    [7444444444444444444, 28.0, 4],
+    [1333333333333333333, 21.0, 5],
+    [4757575577777777777, 5.0, 6],
+    [5555555555555555557, 5.5, 7],
+]
+chtype_map = {
+    1: [ChannelType.TIME, ChannelType.ENERGY],
+    2: [ChannelType.TIME, ChannelType.ENERGY],
+    3: [ChannelType.TIME, ChannelType.ENERGY],
+    4: [ChannelType.TIME, ChannelType.ENERGY],
+    5: [ChannelType.TIME, ChannelType.ENERGY],
+    6: [ChannelType.TIME, ChannelType.ENERGY],
+    7: [ChannelType.TIME, ChannelType.ENERGY],
+}
 
 
 def test_filter_total_energy():
+    assert filter_total_energy(50)
+    assert filter_total_energy(10.1)
+    assert filter_total_energy(99.9)
     assert not filter_total_energy(10)
     assert not filter_total_energy(100)
-    assert filter_total_energy(10.01)
-    assert filter_total_energy(99.99)
-    assert filter_total_energy(50)
-    assert not filter_total_energy(5)
-    assert not filter_total_energy(105)
+    assert not filter_total_energy(9)
+    assert not filter_total_energy(101)
 
 
 def test_filter_min_ch():
-    assert filter_min_ch([1, 2, 3], 3)
-    assert filter_min_ch([1, 2, 3, 4], 3)
-    assert not filter_min_ch([1, 2], 3)
-    assert not filter_min_ch([], 1)
+    assert filter_min_ch(det_list, 6, chtype_map)
+    assert not filter_min_ch(det_list, 10, chtype_map)
 
 
 def test_filter_single_mM():
@@ -47,7 +66,7 @@ def test_filter_max_sm():
     sm_mM_map = {3: 1, 6: 2, 9: 3, 12: 4, 15: 5, 18: 6}
 
     assert not filter_max_sm(det1_list, det2_list, 3, sm_mM_map)
-    assert filter_max_sm(det1_list, det2_list, 5, sm_mM_map)
+    assert filter_max_sm(det1_list, det2_list, 6, sm_mM_map)
 
 
 def test_filter_specific_mm():
@@ -67,3 +86,10 @@ def test_filter_channel_list():
 
     assert not filter_channel_list(det1_list, det2_list, invalid_channels)
     assert filter_channel_list(det1_list, det2_list, valid_channels)
+
+
+def test_filter_ROI():
+    assert filter_ROI(5, 5, (0, 10), (0, 10))
+    assert not filter_ROI(15, 15, (0, 10), (0, 10))
+    assert not filter_ROI(10, 10, (0, 10), (0, 10))
+    assert not filter_ROI(0, 0, (0, 10), (0, 10))
