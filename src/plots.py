88d3aa131ee18571dc74432cbd9451f532958a82
darento <dsartiom@gmail.com>
2024-02-29 10:32:15 +0100

    Added plot_xy_projection function to plot projection on x/y axis, returning 1D hist.

--- src/plots.py
+++ src/plots.py
@@ -69,7 +69,9 @@ def plot_floodmap(
     x, y = zip(*xy_list)
 
     fig = plt.figure(figsize=(10, 10))
-    plt.hist2d(x, y, bins=bins, range=[[0, 26], [0, 26]], cmap="plasma")
+    h, x_edges, y_edges, _ = plt.hist2d(
+        x, y, bins=bins, range=[[0, 26], [0, 26]], cmap="plasma"
+    )
 
     # Set plot properties
     plt.xlabel("X position (mm)")
@@ -83,6 +85,7 @@ def plot_floodmap(
     else:
         plt.clf()
         plt.close()
+    return h, x_edges, y_edges
 
 
 def plot_floodmap_mM(floodmap: dict, bins: tuple = (200, 200)) -> None:
@@ -199,3 +202,64 @@ def plot_event_impact(impact_matrix: np.ndarray) -> None:
     plt.imshow(impact_matrix, cmap="binary", interpolation="nearest")
     plt.colorbar(label="Energy")
     plt.show()
+
+
+def plot_xy_projection(
+    h: np.ndarray,
+    x_edges: np.ndarray,
+    y_edges: np.ndarray,
+    projection_axis: str = "y",
+    show_fig: bool = False,
+) -> None:
+    """
+    This function plots the 1D projection of the 2D histogram for the ROI.
+
+    Parameters:
+        - h (np.ndarray): The 2D histogram.
+        - x_edges (np.ndarray): The bin edges for the x-axis.
+        - y_edges (np.ndarray): The bin edges for the y-axis.
+        - projection_axis (str, optional): The axis along which to project the histogram. Defaults to "y".
+        - show_fig (bool, optional): A flag to show the figure. Defaults to False.
+
+    Returns:
+        - np.ndarray: The 1D projection of the 2D histogram.
+        - np.ndarray: The bin centers for the 1D projection.
+    """
+    # TODO: Maybe ROI directly here instead on the data filter?
+
+    # Find the indices of the y bins that correspond to the ROI
+    # y_min_index = np.searchsorted(y_edges, 11.5, side="right") - 1
+    # y_max_index = np.searchsorted(y_edges, 14.0, side="right") - 1
+    # Find the indices of the x bins that correspond to the ROI
+    # x_min_index = np.searchsorted(x_edges, 0, side="right") - 1
+    # x_max_index = np.searchsorted(x_edges, 16, side="right") - 1
+
+    # Sum the 2D histogram 'h' over the y-axis within the ROI to get a 1D projection
+    # h_projection = np.sum(h[x_min_index:x_max_index, y_min_index:y_max_index], axis=1)
+
+    # The x-values for the projection are the center of the x-bins
+    # x_bin_centers = (x_edges[x_min_index:x_max_index]) / 2
+
+    # width = x_bin_centers[1] - x_bin_centers[0]
+
+    if projection_axis == "y":
+        h_projection = np.sum(h, axis=1)
+        bin_centers = (x_edges[1:] + x_edges[:-1]) / 2
+        xy_projection = "X"
+    elif projection_axis == "x":
+        h_projection = np.sum(h, axis=0)
+        bin_centers = (y_edges[1:] + y_edges[:-1]) / 2
+        xy_projection = "Y"
+    width = bin_centers[1] - bin_centers[0]
+
+    # Now we can plot the 1D projection
+    plt.bar(bin_centers, h_projection, width=width, align="center")
+    plt.xlabel(f"{xy_projection} position (mm)")
+    plt.ylabel("Summed counts in ROI")
+    plt.title("1D Projection of 2D Histogram")
+    if show_fig:
+        plt.show()
+    else:
+        plt.clf()
+        plt.close()
+    return bin_centers, h_projection
